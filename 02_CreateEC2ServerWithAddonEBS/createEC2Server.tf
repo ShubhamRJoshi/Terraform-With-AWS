7df4762053f11f1a06b4f75191667f678749ec2a
ShubhamRJoshi <joshis2991996@gmail.com>
2021-03-08 15:00:33 +0530

    aws volume attachment

--- 02_CreateEC2ServerWithAddonEBS/createEC2Server.tf
+++ 02_CreateEC2ServerWithAddonEBS/createEC2Server.tf
@@ -32,3 +32,13 @@ resource "aws_ebs_volume" "ebs_volume" {
 		Day = var.day
 	}
 }
+
+resource "aws_volume_attachment" "ebs_volume_attachment" {
+	count = var.servers
+	volume_id = aws_ebs_volume.ebs_volume[count.index].id
+	instance_id = aws_instance.ec2_server[count.index].id
+	skip_destroy = true
+	device_name = "/dev/sdf"
+}
+
+# terraform apply --auto-approve -var servers=2
